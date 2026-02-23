25e5b83a92665a21a2fea24ca93f62bc3bf1418b
WalterAurelio <aureliogareca.dev@gmail.com>
2026-02-23 16:36:15 -0300

    Se implementó el componente Button. Se implementó el utils cn.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Button from './components/Button';
+
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1>App</h1>
+      <Button buttonStyle={'fill'} colour={'red'} state={'disabled'}>Button</Button>
+    </div>
+  );
 }
-export default App
+export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,67 @@
-// import { cva, VariantProps } from 'class-variance-authority';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '../utils/cn';
 
-/* const variants = cva(['inline-flex h-9 min-w-28.5 px-3 justify-center items-center gap-2 rounded-full bg-background-neutral-inverse-primary'], {
-  variants: {},
-  // compoundVariants: {},
-  defaultVariants: {}
-}); */
+const variants = cva(['inline-flex', 'h-9', 'min-w-28.5', 'px-m', 'justify-center', 'items-center', 'gap-s', 'rounded-full', 'body-regular'], {
+  variants: {
+    colour: {
+      black: '',
+      red: ''
+    },
+    buttonStyle: {
+      fill: 'text-text-neutral-inverse-primary',
+      outline: 'border-custom-s lg:hover:border-custom-m',
+      minimal: 'lg:hover:border-b-custom-s'
+    },
+    state: {
+      idle: 'lg:cursor-pointer',
+      disabled: ''
+    }
+  },
+  compoundVariants: [
+    {
+      colour: 'black',
+      buttonStyle: 'fill',
+      state: 'idle',
+      className: 'bg-background-neutral-inverse-primary lg:hover:bg-background-neutral-inverse-secondary'
+    },
+    {
+      colour: 'red',
+      buttonStyle: 'fill',
+      state: 'idle',
+      className: 'bg-background-brand-primary lg:hover:bg-background-brand-secondary'
+    },
+    {
+      colour: 'black',
+      buttonStyle: ['outline', 'minimal'],
+      state: 'idle',
+      className: 'text-text-neutral-primary'
+    },
+    {
+      colour: 'red',
+      buttonStyle: ['outline', 'minimal'],
+      state: 'idle',
+      className: 'text-text-brand-primary'
+    },
+    {
+      colour: ['black', 'red'],
+      buttonStyle: ['fill', 'outline', 'minimal'],
+      state: 'disabled',
+      className: 'bg-background-neutral-disabled text-text-neutral-disabled'
+    }
+  ],
+  defaultVariants: {
+    colour: 'black',
+    buttonStyle: 'fill',
+    state: 'idle'
+  }
+});
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof variants>;
 
-function Button({ children, ...props }: ButtonProps) {
+function Button({ colour, buttonStyle, state, children, ...props }: ButtonProps) {
   return (
     <button
-      className=''
+      className={cn(variants({ colour, buttonStyle, state }))}
       {...props}
     >
       {children}
