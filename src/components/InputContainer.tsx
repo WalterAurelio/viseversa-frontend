a1654992bb12dd04071b3d5ef10813a222671bbf
WalterAurelio <aureliogareca.dev@gmail.com>
2026-03-01 13:49:32 -0300

    Se implementó el tipo WithClassName. Se modificó el componente InputContainer para recibir por props el atributo htmlFor.

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -3,13 +3,19 @@ import Dummy from './Dummy';
 
 type InputContainerProps = PropsWithChildren & {
   label?: string;
+  htmlFor: string;
   errorMsg?: string;
 };
 
-function InputContainer({ label, errorMsg, children }: InputContainerProps) {
+function InputContainer({ label, htmlFor, errorMsg, children }: InputContainerProps) {
   return (
     <div className='flex flex-col gap-s'>
-      {label && <label className='text-text-neutral-primary body-bold'>{label}</label>}
+      <label
+        htmlFor={htmlFor}
+        className='text-text-neutral-primary body-bold select-none'
+      >
+        {label}
+      </label>
       {children ? children : <Dummy />}
       {errorMsg && <p className='text-text-brand-primary small-normal'>{errorMsg}</p>}
     </div>
