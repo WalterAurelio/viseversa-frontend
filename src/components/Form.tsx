b53f41d9768ac8cad99d0f99f91840ac12c502d8
WalterAurelio <aureliogareca.dev@gmail.com>
2026-08-23 23:22:25 -0300

    feat: enhance Form component with Zod validation and integrate FormProvider; update stories to demonstrate new functionality

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,27 @@
-type FormProps = React.FormHTMLAttributes<HTMLFormElement>;
+import { useForm, FormProvider } from "react-hook-form";
+import * as z4 from "zod/v4/core";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "../utils/cn";
 
-function Form({ children, ...props }: FormProps) {
+type FormProps<T extends z4.$ZodObject> = React.ComponentPropsWithoutRef<"form"> & {
+  schema: T;
+};
+
+function Form<T extends z4.$ZodObject>({ schema, className, children, ...props }: FormProps<T>) {
+  const methods = useForm({
+    resolver: zodResolver(schema)
+  });
+
+  const onSubmit = (data: z4.infer<T>) => {
+    console.log("Form submitted", data);
+  };
+
   return (
-    <form
-      className='flex p-l flex-col justify-center items-center gap-l rounded-border-xl bg-neutral-primary lg:p-xl'
-      {...props}
-    >
-      {children}
-    </form>
+    <FormProvider {...methods}>
+      <form className={cn("flex flex-col gap-m border p-2xl", className)} onSubmit={methods.handleSubmit(onSubmit)} {...props}>
+        {children}
+      </form>
+    </FormProvider>
   );
 }
 export default Form;
--- src/stories/Form.stories.tsx
+++ src/stories/Form.stories.tsx
@@ -1,14 +1,21 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
-import Form from '../components/Form';
-import Input from '../components/Input';
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import Form from "../components/Form";
+import InputContainer from "../components/InputContainer";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { z } from "zod";
+import Fieldset from "../components/Fieldset";
+import Checkbox from "../components/Checkbox";
+
+const schema = z.object({
+  name: z.string().min(1, { message: "El nombre es requerido" }),
+  lastName: z.string().min(1, { message: "El apellido es requerido" }),
+  size: z.array(z.enum(["XS", "S", "M", "L", "XL", "XXL"]), "Debes seleccionar al menos un talle").nonempty("Debes seleccionar al menos un talle")
+});
 
 const meta = {
-  title: 'Components/Form',
-  component: Form,
-  parameters: {
-    layout: 'centered',
-  },
-  tags: ['autodocs'],
+  title: "Components/Form",
+  component: Form
 } satisfies Meta<typeof Form>;
 
 export default meta;
@@ -16,13 +23,26 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
+    schema,
+    className: "w-96",
     children: (
-      <div className='flex flex-col gap-2'>
-        <Input placeholder='Ingresar nombre' />
-        <Input placeholder='Ingresar apellido' />
-        <Input placeholder='Ingresar usuario' />
-        <Input placeholder='Ingresar email' />
-      </div>
-    ),
-  },
+      <>
+        <InputContainer label="Nombre:" htmlFor="name">
+          <Input type="text" id="name" name="name" placeholder="Ingresa tu nombre" className="w-full" />
+        </InputContainer>
+        <InputContainer label="Apellido:" htmlFor="lastName">
+          <Input type="text" id="lastName" name="lastName" placeholder="Ingresa tu apellido" className="w-full" />
+        </InputContainer>
+        <Fieldset legend="Talle:" htmlName="size">
+          <Checkbox name="size" id="size-xs" label="XS" value="XS" />
+          <Checkbox name="size" id="size-s" label="S" value="S" />
+          <Checkbox name="size" id="size-m" label="M" value="M" />
+          <Checkbox name="size" id="size-l" label="L" value="L" />
+          <Checkbox name="size" id="size-xl" label="XL" value="XL" />
+          <Checkbox name="size" id="size-xxl" label="XXL" value="XXL" />
+        </Fieldset>
+        <Button type="submit">Enviar</Button>
+      </>
+    )
+  }
 };
