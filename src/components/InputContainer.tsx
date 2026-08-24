6d0fa359f0cbbf5904cd73169fb6cd00d7c2d15c
WalterAurelio <aureliogareca.dev@gmail.com>
2026-08-23 23:22:04 -0300

    feat: update InputContainer component to enforce required props and integrate error handling; adjust stories for consistency

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -1,17 +1,22 @@
 import type { PropsWithChildren } from "react";
 import { cn } from "../utils/cn";
+import { useFormContext } from "react-hook-form";
 
 type InputContainerProps = PropsWithChildren<{
   className?: string;
-  label?: string;
-  htmlFor?: string;
-  errorMsg?: string;
+  label: string;
+  htmlFor: string;
 }>;
 
-function InputContainer({ className, label, htmlFor, errorMsg, children }: InputContainerProps) {
+function InputContainer({ className, label, htmlFor, children }: InputContainerProps) {
+  const {
+    formState: { errors }
+  } = useFormContext();
+  const errorMsg = errors[htmlFor]?.message as string | undefined;
+
   return (
     <div className={cn("flex w-full flex-col items-start gap-xs", className)}>
-      <label className={cn("label-default text-neutral-primary", { hidden: !label })} htmlFor={htmlFor}>
+      <label className="label-default text-neutral-primary" htmlFor={htmlFor}>
         {label}
       </label>
       {children}
--- src/stories/InputContainer.stories.tsx
+++ src/stories/InputContainer.stories.tsx
@@ -1,16 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import InputContainer from "../components/InputContainer";
 import Input from "../components/Input";
+import FormWrapper from "../components/FormWrapper";
 
 const meta = {
   title: "Components/InputContainer",
   component: InputContainer,
   args: {
     label: "Label",
-    htmlFor: "input-id",
-    errorMsg: "Error message",
-    children: <Input id="input-id" placeholder="Placeholder" />
-  }
+    htmlFor: "input",
+    children: <Input name="input" id="input" placeholder="Placeholder" />
+  },
+  decorators: [
+    (Story) => (
+      <FormWrapper name="input">
+        <Story />
+      </FormWrapper>
+    )
+  ]
 } satisfies Meta<typeof InputContainer>;
 
 export default meta;
