5722c3aa4a61969fa533b828e831701bd56ee586
WalterAurelio <aureliogareca.dev@gmail.com>
2026-08-23 23:16:16 -0300

    feat: enhance Checkbox component to support form integration; add Checkbox stories for demonstration

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,16 +1,22 @@
+import { useFormContext } from "react-hook-form";
 import Check from "../assets/icons/Check.svg?react";
 
 type CheckboxProps = React.ComponentProps<"input"> & {
+  name: string;
+  id: string;
   label: string;
+  value: string;
 };
 
-function Checkbox({ label = "Label", id = "some-id", ...props }: CheckboxProps) {
+function Checkbox({ name, id, label, value, ...props }: CheckboxProps) {
+  const { register } = useFormContext();
+
   return (
     <label
       htmlFor={id}
       className="group inline-flex h-10 cursor-pointer items-center justify-center gap-s rounded-border-l border border-neutral-inverse-primary bg-neutral-primary px-m py-s label-default select-none"
     >
-      <input type="checkbox" id={id} name={label} className="sr-only" {...props} />
+      <input {...register(name)} id={id} type="checkbox" value={value} className="sr-only" {...props} />
       <span className="flex size-5 items-center justify-center rounded-border-s bg-neutral-disabled group-has-checked:bg-neutral-inverse-primary">
         <Check className="hidden size-3 text-neutral-inverse-primary group-has-checked:block" />
       </span>
--- src/stories/Checkbox.stories.tsx
+++ src/stories/Checkbox.stories.tsx
@@ -1,13 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import Checkbox from "../components/Checkbox";
+import FormWrapper from "../components/FormWrapper";
 
 const meta = {
   title: "Components/Checkbox",
   component: Checkbox,
   args: {
+    name: "checkbox",
+    id: "checkbox",
     label: "Label",
-    id: "some-id"
-  }
+    value: "checkbox"
+  },
+  decorators: [
+    (Story) => (
+      <FormWrapper name="checkbox">
+        <Story />
+      </FormWrapper>
+    )
+  ]
 } satisfies Meta<typeof Checkbox>;
 
 export default meta;
