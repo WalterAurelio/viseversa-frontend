f9c0361a4f992d512d67763c0925b7a34fa94287
WalterAurelio <aureliogareca.dev@gmail.com>
2026-09-07 16:33:13 -0300

    feat: add Select and Option components to createForm and update testSchema with province field

--- src/utils/createForm.ts
+++ src/utils/createForm.ts
@@ -4,6 +4,8 @@ import InputContainerComponent from "../components/InputContainer";
 import InputComponent from "../components/Input";
 import FieldsetComponent from "../components/Fieldset";
 import CheckboxComponent from "../components/Checkbox";
+import SelectComponent from "../components/Select";
+import OptionComponent from "../components/Option";
 
 export function createForm<T extends z4.$ZodObject>(schema: T) {
   type FormData = z4.infer<T>;
@@ -14,6 +16,8 @@ export function createForm<T extends z4.$ZodObject>(schema: T) {
   const Input = InputComponent<FormData>;
   const Fieldset = FieldsetComponent<FormData>;
   const Checkbox = CheckboxComponent<FormData>();
+  const Select = SelectComponent<FormData>;
+  const Option = OptionComponent<FormData>();
 
-  return { Form, InputContainer, Input, Fieldset, Checkbox };
+  return { Form, InputContainer, Input, Fieldset, Checkbox, Select, Option };
 }
