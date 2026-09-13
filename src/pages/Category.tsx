af809979ee3541e29cfce316a05a2bdbf2a2a314
WalterAurelio <aureliogareca.dev@gmail.com>
2026-09-12 21:39:36 -0300

    feat: refactor Category component to use getProductsByCategory and update rendering logic

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,26 +1,26 @@
-import { useParams,useSearchParams } from 'react-router';
-import Filters from '../components/Filters';
-import { useFilterProducts } from '../services/products/products.queries';
-import type { ProductHomeResponse } from '../services/products/products.types';
+import { useParams, useSearchParams } from "react-router";
+import { useGetProductsByCategory } from "../services/products/products.queries";
+import Filter from "../components/Filter";
 
 function Category() {
-  const { categoria } = useParams()
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { data: products } = useFilterProducts(categoria ?? '', searchParams.toString());
-  
+  const { category } = useParams();
+  const [searchParams] = useSearchParams();
+  const { data } = useGetProductsByCategory(category!, searchParams.toString());
+
   return (
     <div>
-      <>{Filters(setSearchParams)}</>
-      {products.map((p: ProductHomeResponse, idx: number) => (
-        <div key={idx} className="w-28">
-          <img src={`${p.imagenes[0]}`} alt="" />
-          <h2>{p.titulo}</h2>
-          <p>{p.descripcion}</p>
-          <p>{p.ubicacion}</p>
-        </div>
-      ))}
+      <h1>Categoría: {category}</h1>
+      <Filter />
+      <div>
+        {data.map((product, index) => (
+          <div key={index} className="w-28">
+            <p>{product.title}</p>
+            <p>{product.description}</p>
+            <img src={`${product.image}`} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
 export default Category;
